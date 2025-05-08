638ac76b932f9ce9f5016c41547f9cb8deb1c14b
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2025-05-08 15:02:30 +0200

    test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '../style/global.css'
+    import Header from '../layout/Header.svelte';
+    import type { Snippet } from 'svelte';
+
+    const { children }: {
+        children: Snippet
+    } = $props()
+
+</script>
+
+<Header />
+<main>
+    {@render children()}
+</main>
+<footer></footer>
