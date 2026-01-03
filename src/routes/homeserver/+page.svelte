3fc6c704d8f7a9fe72d8406158f8bb13480f5b26
dugalcedo <dougalcedo@gmail.com>
2026-01-03 17:39:13 +0100

    hs redirect

--- src/routes/homeserver/+page.svelte
+++ src/routes/homeserver/+page.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
     import { onMount } from "svelte";
     onMount(() => {
-        window.location.href = "https://vivien-ruinous-efferently.ngrok-free.dev/"
+        let flag = "/homeserver/"
+        let href = window.location.href;
+        let i = href.indexOf(flag) + flag.length
+        window.location.href = `https://vivien-ruinous-efferently.ngrok-free.dev/${href.slice(i)}`
     })
 </script>
